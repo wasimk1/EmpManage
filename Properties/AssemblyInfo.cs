0d3880d22e790fa4b3f2b7bd4cc021c6c74a27f4
Wasim Khan <wasimkhan0816@gmail.com>
2024-07-04 13:46:54 +0530

    AssemblyInfo.cs file version added = 1.1.0801.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Leave Management")]
+[assembly: AssemblyTitle("EmpManage")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Leave Management")]
+[assembly: AssemblyProduct("EmpManage")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.7.24.13")]
-[assembly: AssemblyFileVersion("4.7.24.13")]
+[assembly: AssemblyVersion("1.1.0801.1")]
+[assembly: AssemblyFileVersion("1.1.0801.1")]
